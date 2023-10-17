b04b167e57a7afb6e231b9fc8b774de5729e254a
Royweru <royezi17@gmail.com>
2023-10-17 09:17:59 +0300

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   app/(auth)/layout.tsx 	new file:   app/(base)/(routes)/page.tsx 	new file:   app/favicon.ico 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   components.json 	new file:   components/Navbar.tsx 	new file:   lib/utils.ts 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   tailwind.config.js 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ app/(base)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+     <div className='text-2xl text-yellow-200 font-bold '>
+      Welcome to Trucommerce
+     </div>
+    </main>
+  )
+}
